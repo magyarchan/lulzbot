b36796a657fa3679e54e8ee1c84baedaed325644
jaksi07c8 <jaksi07c8@gmail.com>
2014-01-20 20:22:54 +0100

    -command substitution implementalva -CR es LF karakterek kiszedese a kuldendo szovegbol -garoi admazsolfa

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import re
 import irc.bot
 import irc.strings
 
@@ -28,7 +29,7 @@ class LulzBot(irc.bot.SingleServerIRCBot):
         message = e.arguments[0]
         if message[0] == '!':
             self.do_command(e)
-        # TODO: ezt itt lent kiegesziteni egy whitelisttel, amit egy adatbazis tablabol olvasunk befele
+            # TODO: ezt itt lent kiegesziteni egy whitelisttel, amit egy adatbazis tablabol olvasunk befele
         for url in message.split():
             title = urlparser.get_title(url)
             if title:
@@ -56,7 +57,7 @@ class LulzBot(irc.bot.SingleServerIRCBot):
 
     def say(self, target, message):
         for i in range(0, len(message), 350):
-            self.connection.privmsg(target, '\x0305' + message[i:i + 350])
+            self.connection.privmsg(target, '\x0305' + message[i:i + 350].replace('\r', '').replace('\n', ' '))
 
     def say_public(self, message):
         self.say(self.channel, message)
@@ -69,20 +70,64 @@ class LulzBot(irc.bot.SingleServerIRCBot):
 
     def do_command(self, e):
         command = e.arguments[0].split()[0].lower()
-        arguments = e.arguments[0].split()[1:]
+        arguments = ' '.join(e.arguments[0].split()[1:])
         if command[0] == '!':
             command = command[1:]
-        try:
-            cmd_handler = getattr(commands, 'cmd_' + command)
-        except AttributeError:
-            pass
+        cnt = 0
+        escape = False
+        for char in arguments:
+            if char == '\\':
+                escape = not escape
+            elif char == '{' and not escape:
+                cnt += 1
+            elif char == '}' and not escape:
+                cnt -= 1
+            else:
+                escape = False
+            if cnt < 0:
+                break
+        if cnt != 0:
+            self.reply(e, 'Páratlan zárójelek!')
         else:
-            self.reply(e, cmd_handler(arguments))
+            while re.search(r'^{', arguments) or re.search(r'[^\\]{', arguments):
+                if re.search(r'[^\\]({)', arguments):
+                    match = None
+                    for match in re.finditer(r'[^\\]({)', arguments):
+                        pass
+                    if match:
+                        i1 = match.start(1)
+                    else:
+                        i1 = 0
+                else:
+                    i1 = 0
+                match = re.search(r'[^\\](})', arguments[i1:])
+                i2 = i1 + match.start(1)
+                if i1 + 1 == i2:
+                    self.reply(e, 'Hogy mit?')
+                    return
+                subcommand = arguments[i1 + 1:i2].split()[0].lower()
+                subarguments = ' '.join(arguments[i1 + 1:i2].split()[1:]).\
+                    replace('\\{', '{').replace('\\}', '}').replace('\\\\', '\\')
+                try:
+                    cmd_handler = getattr(commands, 'cmd_' + subcommand)
+                except AttributeError:
+                    self.reply(e, 'Hogy mit?')
+                    return
+                else:
+                    subresult = cmd_handler(subarguments).replace('\\', '\\\\').replace('{', '\\{').replace('}', '\\}')
+                    arguments = arguments[:i1] + subresult + arguments[i2 + 1:]
+            try:
+                cmd_handler = getattr(commands, 'cmd_' + command)
+            except AttributeError:
+                self.reply(e, 'Hogy mit?')
+            else:
+                self.reply(e, cmd_handler(arguments.replace('\\{', '{').replace('\\}', '}').replace('\\\\', '\\')))
 
 
 def main():
     bot = LulzBot()
     bot.start()
+
 
 if __name__ == "__main__":
     main()
