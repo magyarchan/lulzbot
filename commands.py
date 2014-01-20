3a7520e6b32c57ed052e632b87e0917a920eee1d
redacted <redacted>
2014-01-20 20:22:54 +0100

    -command substitution implementalva -CR es LF karakterek kiszedese a kuldendo szovegbol -garoi admazsolfa

--- commands.py
+++ commands.py
@@ -4,11 +4,12 @@ import sys
 
 def cmd_help(args):
     """Te mit?"""
-    if len(args) == 0:
-        return 'Parancsok: ' + ', '.join([x[0][4:] for x in inspect.getmembers(sys.modules[__name__], inspect.isfunction) if x[0][:4] == 'cmd_'])
+    if not args:
+        return 'Parancsok: ' + ', '.join(
+            [x[0][4:] for x in inspect.getmembers(sys.modules[__name__], inspect.isfunction) if x[0][:4] == 'cmd_'])
     else:
         try:
-            cmd_handler = getattr(sys.modules[__name__], 'cmd_' + args[0])
+            cmd_handler = getattr(sys.modules[__name__], 'cmd_' + args)
         except AttributeError:
             return 'Nincs ilyen parancs :C'
         else:
@@ -21,3 +22,50 @@ def cmd_help(args):
 def cmd_ping(args):
     """Pong!!!"""
     return 'Pong!'
+
+
+def cmd_garoi(args):
+    rules = [
+        ('ddzs', 'CCS'),
+        ('dzs',  'CS'),
+        ('ccs',  'DDZS'),
+        ('ddz',  'TSSZ'),
+        ('ggy',  'TTY'),
+        ('lly',  'JJ'),
+        ('ssz',  'ZZ'),
+        ('tty',  'GGY'),
+        ('zzs',  'SS'),
+        ('dj',   'gy'),
+        ('sz',   'Z'),
+        ('ch',   'cs'),
+        ('ts',   'cs'),
+        ('cs',   'DZS'),
+        ('dz',   'TSZ'),
+        ('gy',   'TY'),
+        ('ly',   'J'),
+        ('cc',   'TSSZ'),
+        ('ty',   'GY'),
+        ('zs',   'S'),
+        ('jj',   'LLY'),
+        ('zz',   'SSZ'),
+        ('b',    'P'),
+        ('c',    'TSZ'),
+        ('d',    'T'),
+        ('f',    'V'),
+        ('g',    'K'),
+        ('j',    'LY'),
+        ('k',    'G'),
+        ('p',    'B'),
+        ('s',    'ZS'),
+        ('s',    'ZZS'),
+        ('t',    'D'),
+        ('v',    'F'),
+        ('z',    'SZ'),
+        ('x',    'GZ')]
+    if not args:
+        return 'Mid agarz?'
+    else:
+        text = args.lower()
+        for _f, _t in rules:
+            text = text.replace(_f, _t)
+        return text.lower()
