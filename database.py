d85bcec2cd601626789da14c4c70271565aee906
redacted <redacted>
2014-02-15 13:12:35 +0100

    -adatbazis hasznalata sqlalchemy-vel -koszontes csatornaba belepeskor -nickvaltas ha az eredeti nevu bot timeoutol -parancs jogosultsagkezeles -felhasznalo-, regex-pattern- es udvozletkezelo parancsok

---
+++ database.py
@@ -0,0 +1,42 @@
+import sqlalchemy
+import sqlalchemy.schema
+import sqlalchemy.orm
+import sqlalchemy.orm.session
+import sqlalchemy.ext.declarative
+
+import database_config
+
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+session = sqlalchemy.orm.session.Session()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String(32), unique=True)
+
+
+class Pattern(Base):
+    __tablename__ = 'patterns'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    user = sqlalchemy.orm.relationship('User', backref=sqlalchemy.orm.backref('patterns'))
+    pattern = sqlalchemy.Column(sqlalchemy.String(64))
+    __table_args__ = (sqlalchemy.schema.UniqueConstraint('user_id', 'pattern'),)
+
+
+class Welcome(Base):
+    __tablename__ = 'welcomes'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    user = sqlalchemy.orm.relationship('User', backref=sqlalchemy.orm.backref('welcomes'))
+    welcome = sqlalchemy.Column(sqlalchemy.String(128))
+    __table_args__ = (sqlalchemy.schema.UniqueConstraint('user_id', 'welcome'),)
+
+
+def initialize():
+    global session
+    engine = sqlalchemy.create_engine(database_config.connection_string)
+    _session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = _session()
