a19ee8e135ad47574c93370aa114b0ee47494eba
aldum <aldum@sch.bme.hu>
2016-06-23 19:58:09 +0200

    #15 urlparser cleaned somewhat

--- urlparser.py
+++ urlparser.py
@@ -6,7 +6,7 @@ import bs4
 def get_title(url):
     try:
         page = urllib.request.urlopen(url)
-        bs = bs4.BeautifulSoup(page)
+        bs = bs4.BeautifulSoup(page, "html.parser")
         if bs.title:
             return bs.title.string.strip()
     except:
