fca5d97fe77a22057de76cde41eebce05ca8dff3
aldum <aldum@sch.bme.hu>
2016-09-19 23:00:22 +0200

    urlparser HEAD prerequest for error handling

--- urlparser.py
+++ urlparser.py
@@ -5,10 +5,15 @@ import bs4
 
 def get_title(url):
     try:
-        page = urllib.request.urlopen(url)
-        bs = bs4.BeautifulSoup(page, "html.parser")
-        if bs.title:
-            return bs.title.string.strip()
-    except:
-        print('bazdmeg')
-        pass
+        request = urllib.request.Request(url, method='HEAD')
+        head = urllib.request.urlopen(request, timeout = 3)
+        if head.code == 200:
+            page = urllib.request.urlopen(url)
+            bs = bs4.BeautifulSoup(page, "html.parser")
+            if bs.title:
+                return bs.title.string.strip()
+        else:
+            return ''
+    except Exception as e:
+        print('bazdmeg: ' + e.reason)
+        return ''
